6dd2b5a094e4a0bac7c3e95a3ff051c942455979
Feras Deiratany <deiratany.1@osu.edu>
2016-06-23 22:56:48 -0400

    start bot

---
+++ helloworldbot.js
@@ -0,0 +1,15 @@
+var TWITTER_CONSUMER_KEY = 'ENTER KEY HERE';
+var TWITTER_CONSUMER_SECRET = 'Enter SECRET HERE';
+var TWITTER_ACCESS_TOKEN = 'ENTER TOKEN HERE';
+var TWITTER_ACCESS_TOKEN_SECRET = 'ENTER TOKEN SECRET HERE';
+
+var Twit = require('twit');
+
+var T = new Twit({
+    consumer_key: TWITTER_CONSUMER_KEY,
+    consumer_secret: TWITTER_CONSUMER_SECRET,
+    access_token: TWITTER_ACCESS_TOKEN,
+    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+});
+
+console.log('hello world bot is running...');
